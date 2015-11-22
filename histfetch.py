e6e2215098e219b92ef4a1e9814e07ac8f91de70
Matvey Zaharchenko <matvey.ff@gmail.com>
2015-11-22 22:53:20 +0300

    Add config file

--- histfetch.py
+++ histfetch.py
@@ -5,11 +5,13 @@ import sys
 import glob
 import os
 import re
+import json
 
 import address_storage
 
 dbpath = os.path.expanduser("~/.mozilla/firefox/*.default/places.sqlite")
 dbpaths = glob.glob(dbpath)
+config_path = os.path.expanduser("~/.config/histfetch/config.json")
 
 def isSQLite3(filename):
     from os.path import isfile, getsize
@@ -36,17 +38,26 @@ def import_from_database(path, address_storage):
     for response in cursor:
         address_storage.feed(response[0], response[1])
 
-def main():
-    dictionary_address = "www.oxfordlearnersdictionaries.com"
-    address_patterns = [
-        (
-            re.compile(dictionary_address),
-            [
-                re.compile(".*/definition/.*?q=(.*)"),
-                re.compile(".*/definition/.*/(.*)#"),
-            ]
+def compile_address_patterns(config):
+    address_patterns = []
+    for pattern_pair in config["patterns"]:
+        address_patterns.append(
+            (
+                re.compile(pattern_pair["dictionary_address"]),
+                [re.compile(word_pattern)
+                    for word_pattern in pattern_pair["word_patterns"]]
+            )
         )
-    ]
+    return address_patterns
+
+def main():
+    try:
+        with open(config_path, "rt") as config_file:
+            config = json.load(config_file)
+        address_patterns = compile_address_patterns(config)
+    except:
+        print("Can't read config file:", sys.exc_info(), file=sys.stderr)
+        exit(1)
     storage = address_storage.AddressStore(address_patterns)
     if not dbpaths:
         print("No paths to import words from", file=sys.stderr)
