5a928a15353881839c468af3d9f64d97f03d08e4
Matvey Zaharchenko <matvey.ff@gmail.com>
2015-11-22 23:19:38 +0300

    Store last used config

--- histfetch.py
+++ histfetch.py
@@ -16,6 +16,7 @@ dbpaths = glob.glob(dbpath)
 config_directory_path = os.path.expanduser("~/.config/histfetch")
 config_path = "/".join([config_directory_path, "config.json"])
 default_config_path = "default_config.json"
+last_config_path = "/".join([config_directory_path, "last_used_config.json"])
 
 def isSQLite3(filename):
     from os.path import isfile, getsize
@@ -57,21 +58,25 @@ def compile_address_patterns(config):
 def read_config_file(config_path):
     with open(config_path, "rt") as config_file:
         config = json.load(config_file)
-    return compile_address_patterns(config)
+    return config
+
+def create_default_config():
+    try:
+        os.makedirs(config_directory_path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            print("Can't create default config:", sys.exc_info(),
+                    file=sys.stderr)
+            exit(1)
+    shutil.copyfile(default_config_path, config_path)
 
 def main():
     if not os.path.isfile(config_path):
         print("No config fine found; create default", file=sys.stderr)
-        try:
-            os.makedirs(config_directory_path)
-        except OSError as exception:
-            if exception.errno != errno.EEXIST:
-                print("Can't create default config:", sys.exc_info(),
-                        file=sys.stderr)
-                exit(1)
-        shutil.copyfile(default_config_path, config_path)
+        create_default_config()
     try:
-        address_patterns = read_config_file(config_path)
+        config = read_config_file(config_path)
+        address_patterns = compile_address_patterns(config)
     except:
         print("Can't read config file:", sys.exc_info(), file=sys.stderr)
         exit(1)
@@ -82,6 +87,8 @@ def main():
         sys.exit(1)
     for path in dbpaths:
         import_from_database(path, storage)
+    with open(last_config_path, "wt") as last_config_file:
+        json.dump(config, last_config_file)
     storage.print_all()
 
 if __name__ == "__main__":
